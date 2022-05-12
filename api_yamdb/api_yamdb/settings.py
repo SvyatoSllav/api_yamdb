01e283676e34ce97a6511707f994e71a13df5234
Александр <tarasov.slavas2002@gmail.com>
2022-05-12 20:58:11 +0000

    deleted mine password

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -152,7 +152,7 @@ SIMPLE_JWT = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "tarasov.slavas2002@gmail.com"
-EMAIL_HOST_PASSWORD = "llamster2002"
+EMAIL_HOST_USER = "our@mail"
+EMAIL_HOST_PASSWORD = "mail password"
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
