a204e7a87ed85e297e8173f868bc6df0c2b17cfa
Александр <tarasov.slavas2002@gmail.com>
2022-05-14 12:15:24 +0000

    Удалил свою почту

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -155,7 +155,7 @@ SIMPLE_JWT = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "tarasov.slavas2002@gmail.com"
-EMAIL_HOST_PASSWORD = "llamster2002"
+EMAIL_HOST_USER = "our@mail"
+EMAIL_HOST_PASSWORD = "password"
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
