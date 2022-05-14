3526d22ab14893d1d8e6ad4d8e1c4330d1c20c44
Александр <tarasov.slavas2002@gmail.com>
2022-05-14 12:12:32 +0000

    Исправил PEP8 ошибку

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -27,8 +27,8 @@ class AuthorModerAdmin(permissions.BasePermission):
                 or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS or
-                obj.author == request.user
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
                 or request.user.role == 'admin'
                 or request.user.role == 'moderator'
                 or request.user.is_superuser)
